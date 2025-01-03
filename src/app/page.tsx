5a04dc1135d7bff4b7ba8ba5fa0f9603f1a81819
M. Faisal Siddiqi <sdq7519@gmail.com>
2025-01-03 23:51:49 +0500

    Update page.tsx
    To prevent error during deployment on Vercel.,2 lines containing components imports for Navbar and Footer were removed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 import Image from "next/image";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+
 export default function Home() {
   return (
 <div className=" bg-black">
